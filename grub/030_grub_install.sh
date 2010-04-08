426f0be4116253924a288e26e00596c2d94cb4ce
Troy Will <troydwill@gmail.com>
2010-04-08 00:02:20 +0000

    modified:   030_grub_install.sh 	new file:   040_grub_config.sh

--- grub/030_grub_install.sh
+++ grub/030_grub_install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+INPUT_DEVICE='/dev/sda'
 OUTPUT_FILE='/tmp/mbr.sda.safety'
 # Safety first: make a copy of mbr
-dd bs=512 count=1 if=/dev/sda of=${OUTPUT_FILE}
+dd bs=512 count=1 ${INPUT_DEVICE}/dev/sda of=${OUTPUT_FILE}
 
 # Make another copy of mbr
 cp --backup ${OUTPUT_FILE} ~
 
 grub-install /dev/sda
+
+dd bs=512 count=1 ${INPUT_DEVICE} /root/mbr.after.grub.install
