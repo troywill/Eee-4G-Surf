123c0d9e2150be9a2a23bfbab71fae7fd170a677
troy <troy@dover.(none)>
2010-05-24 20:21:33 -0700

    modified:   var/lib/Eee-4G-Surf/grub/030_grub_install.sh

--- var/lib/Eee-4G-Surf/grub/030_grub_install.sh
+++ var/lib/Eee-4G-Surf/grub/030_grub_install.sh
@@ -2,11 +2,11 @@
 INPUT_DEVICE='/dev/sda'
 OUTPUT_FILE='/tmp/mbr.sda.safety'
 # Safety first: make a copy of mbr
-dd bs=512 count=1 ${INPUT_DEVICE}/dev/sda of=${OUTPUT_FILE}
+dd bs=512 count=1 if=${INPUT_DEVICE} of=${OUTPUT_FILE}
 
 # Make another copy of mbr
 cp --backup ${OUTPUT_FILE} ~
 
 # grub-install /dev/sda
 grub-install /dev/sdb
-dd bs=512 count=1 ${INPUT_DEVICE} /root/mbr.after.grub.install
+dd bs=512 count=1 if=${INPUT_DEVICE} /root/mbr.after.grub.install
